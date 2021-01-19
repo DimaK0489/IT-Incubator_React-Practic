d959b9e31497b82e6de6207b668957794544143a
DimaK0489 <dim040489@gmail.com>
2021-01-19 21:32:32 +0300

    unit commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import {Rating, RatingValueType} from "./components/Rating/Raiting";
 import {OnOff} from "./components/OnOff/OnOff";
 import {UncontrolledAccordion} from "./components/UncontroledAccordion/UncontroledAccordion";
 import {UncontrolledRating} from "./components/UncontroledRating/UncontroledRaiting";
+import {Accordion} from "./components/accordion/Accordion";
 
 
 export function App() {
     console.log("App rendering")
 
     let [ratingValue, setRatingValue] = useState<RatingValueType>(4)
+    let [accordionCollapsed, setAccordionCollapsed] = useState<boolean>(false)
     return (
         <div className="App">
             <PageTitle title={"This is App component"}/>
@@ -22,6 +24,8 @@ export function App() {
             <div className={"components"}>
                 <Rating value={ratingValue}
                         onClick={setRatingValue}/>
+                <Accordion titleValue={"Menu"}
+                           collapsed={accordionCollapsed}  />
             </div>
         </div>
     );
--- src/components/UncontroledRating/UncontroledRaiting.tsx
+++ src/components/UncontroledRating/UncontroledRaiting.tsx
@@ -12,11 +12,11 @@ export function UncontrolledRating() {
 
     return (
         <div>
-            <Star selected={value > 0} setValue={setValue} value={1}/>
-            <Star selected={value > 1} setValue={setValue} value={2}/>
-            <Star selected={value > 2} setValue={setValue} value={3}/>
-            <Star selected={value > 3} setValue={setValue} value={4}/>
-            <Star selected={value > 4} setValue={setValue} value={5}/>
+            <Star selected={value > 0} setValue={()=>{setValue(1)}} />
+            <Star selected={value > 1} setValue={()=>{setValue(2)}} />
+            <Star selected={value > 2} setValue={()=>{setValue(3)}} />
+            <Star selected={value > 3} setValue={()=>{setValue(4)}} />
+            <Star selected={value > 4} setValue={()=>{setValue(5)}} />
         </div>
     );
 }
@@ -24,15 +24,14 @@ export function UncontrolledRating() {
 
 type StarPropsType = {
     selected: boolean
-    value: 1 | 2 | 3 | 4 | 5
-    setValue: (value: 1 | 2 | 3 | 4 | 5) => void
+    setValue: () => void
 }
 
 function Star(props: StarPropsType) {
     console.log("Star rendering")
 
     // return props.selected ? <span><b>Star </b></span> : <span> Star </span>
-    return <span onClick={ () => { props.setValue(props.value)}}>
+    return <span onClick={ () => { props.setValue()}}>
         {props.selected ? <b> Star </b> : " Star "}
     </span>
 }
